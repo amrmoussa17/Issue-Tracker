bc33f6d147536e4ec3981306c6427d2dda2ec358
Amr Moussa <amrmamdoh@hotmail.com>
2024-05-06 22:21:04 +0300

    Add ReactFormHook to create issue form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,53 @@
-"use client";
-import React from "react";
-import { TextField, Button } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
+"use client"
+import React, { useState } from "react"
+import { TextField, Button, Callout } from "@radix-ui/themes"
+import SimpleMDE from "react-simplemde-editor"
+import "easymde/dist/easymde.min.css"
+import { useForm, SubmitHandler, Controller } from "react-hook-form"
+import axios from "axios"
+import { useRouter } from "next/navigation"
 
-const NewIssuepage = () => {
+interface Inputs {
+  title: string
+  description: string
+}
+
+const NewIssuePage = () => {
+  const [error, setError] = useState("")
+  const { register, handleSubmit, control } = useForm<Inputs>()
+  const router = useRouter()
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      await axios.post("/api/issues", data)
+      router.push("/issues")
+    } catch (error) {
+      setError("An unexpected error occurred.")
+    }
+  }
+
   return (
-    <div className="max-w-xl space-y-4">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE />
-      <Button>Submit New Issue</Button>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-4">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+        <TextField.Root
+          placeholder="Title"
+          {...register("title")}
+        ></TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <Button>Submit New Issue</Button>
+      </form>
     </div>
-  );
-};
+  )
+}
 
-export default NewIssuepage;
+export default NewIssuePage
