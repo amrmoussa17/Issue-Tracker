cf6e0d259b6395548ec48ff70956df6c446fa8a5
Amr Moussa <amrmamdoh@hotmail.com>
2024-05-10 12:02:59 +0300

    Add styling and markdown preview to issue details page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,8 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge"
 import prisma from "@/prisma/client"
+import { Card, Heading } from "@radix-ui/themes"
 import { notFound } from "next/navigation"
-import React from "react"
+import Markdown from "react-markdown"
 
 interface Props {
   params: {
@@ -15,11 +17,15 @@ const IssueDetailPage = async ({ params }: Props) => {
   })
   if (!issue) notFound()
   return (
-    <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+    <div className="space-y-2">
+      <Heading>{issue.title}</Heading>
+      <div className="flex space-x-3">
+        <IssueStatusBadge status={issue.status} />
+        <p>{issue.createdAt.toDateString()}</p>
+      </div>
+      <Card className="prose">
+        <Markdown>{issue.description}</Markdown>
+      </Card>
     </div>
   )
 }
