e36313f35ef57becc865df2f139c25687e09d862
amr moussa <amrmamdoh@hotmail.com>
2024-05-21 14:39:46 +0300

    added sql data file

---
+++ myDataVersion.js
@@ -0,0 +1,123 @@
+const data = [
+  {
+    title: "Website Login Issue",
+    description:
+      "Users are unable to log in to the website They receive an error message when attempting to sign in.",
+    status: "OPEN",
+  },
+  {
+    title: "Product Page Loading Slowly",
+    description:
+      "The product page is taking a long time to load, causing a poor user experience.",
+    status: "IN_PROGRESS",
+  },
+  {
+    title: "Payment Gateway Down",
+    description:
+      "The payment gateway is currently offline, preventing customers from making purchases.",
+    status: "CLOSED",
+  },
+  {
+    title: "Mobile App Crashes on Launch",
+    description:
+      "The mobile app crashes immediately upon launch, making it unusable.",
+    status: "OPEN",
+  },
+  {
+    title: "Order Tracking Not Updating",
+    description:
+      "Customers are unable to track their orders as the tracking information is not being updated.",
+    status: "IN_PROGRESS",
+  },
+  {
+    title: "Missing Product Images",
+    description:
+      "Some product images are missing from the catalog, making it hard for customers to view products.",
+    status: "CLOSED",
+  },
+  {
+    title: "Password Reset Not Working",
+    description:
+      "Users cannot reset their passwords using the Forgot Password feature on the website.",
+    status: "OPEN",
+  },
+
+  {
+    title: "Checkout Process Errors",
+    description:
+      "Customers are encountering errors during the checkout process, preventing them from completing orders.",
+    status: "IN_PROGRESS",
+  },
+  {
+    title: "Broken Links on Homepage",
+    description:
+      "There are broken links on the homepage that need to be fixed for a seamless user experience.",
+    status: "OPEN",
+  },
+  {
+    title: "Inventory Sync Issue",
+    description:
+      "The inventory levels are not syncing correctly, leading to discrepancies between online and offline stock.",
+    status: "IN_PROGRESS",
+  },
+  {
+    title: "Performance Degradation on Server",
+    description:
+      "The server is experiencing performance degradation, causing slow response times.",
+    status: "CLOSED",
+  },
+  {
+    title: "Broken Search Functionality",
+    description:
+      "The search functionality on the website is not returning accurate results or is not working at all.",
+    status: "IN_PROGRESS",
+  },
+  {
+    title: "SSL Certificate Expiry",
+    description:
+      "The SSL certificate is nearing its expiry date and needs to be renewed to ensure secure connections.",
+    status: "IN_PROGRESS",
+  },
+  {
+    title: "Incorrect Product Pricing",
+    description:
+      "Some products are displaying incorrect prices, leading to pricing discrepancies.",
+    status: "CLOSED",
+  },
+  {
+    title: "404 Error on Blog Pages",
+    description:
+      "Visitors are encountering 404 errors when trying to access blog articles on the website.",
+    status: "OPEN",
+  },
+  {
+    title: "Missing User Profile Data",
+    description:
+      "User profile data, such as names and contact information, is missing for some accounts.",
+    status: "CLOSED",
+  },
+  {
+    title: "Customer Support Chat Unavailable",
+    description:
+      "The live chat support feature is currently unavailable, leaving customers without immediate assistance.",
+    status: "IN_PROGRESS",
+  },
+  {
+    title: "Payment Refund Request",
+    description:
+      "A customer has requested a refund for an order, and it needs to be processed promptly.",
+    status: "OPEN",
+  },
+  {
+    title: "Payment Refund Request",
+    description:
+      "A customer has requested a refund for an order, and it needs to be processed promptly.",
+    status: "OPEN",
+  },
+  {
+    title: "Payment Refund Request",
+    description:
+      "A customer has requested a refund for an order, and it needs to be processed promptly.",
+    status: "OPEN",
+  },
+]
