2271268878d5e12cef055c485fa2c9765fda5b69
Amr Moussa <amrmamdoh@hotmail.com>
2024-05-26 21:42:17 +0300

    delete generate metadata function

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -39,12 +39,4 @@ const IssueDetailPage = async ({ params }: Props) => {
   )
 }
 
-export async function generateMetaData({ params }: Props) {
-  const issue = await prisma.issue.findUnique({ where: { id: params.id } })
-  return {
-    title: issue?.title,
-    description: "Details of issue" + issue?.id,
-  }
-}
-
 export default IssueDetailPage
