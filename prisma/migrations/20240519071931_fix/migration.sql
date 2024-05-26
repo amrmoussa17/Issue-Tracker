8aeb4355e6c4b922c5702a78b5fb42cc6b42e67f
Amr Moussa <amrmamdoh@hotmail.com>
2024-05-26 21:27:56 +0300

    Reset migration file to be in one file

--- prisma/migrations/20240519071931_fix/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Issue" DROP CONSTRAINT "Issue_assignedToUserId_fkey";
-
--- AlterTable
-ALTER TABLE "Issue" ALTER COLUMN "assignedToUserId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Issue" ADD CONSTRAINT "Issue_assignedToUserId_fkey" FOREIGN KEY ("assignedToUserId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
